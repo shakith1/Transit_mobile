211061e63135a2dd9028c00c434b399dcef210f1
Shakith <nimsara.shakith@gmail.com>
2020-10-13 16:57:10 +0530

    Home Page

---
+++ app/src/main/java/com/example/transit/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.transit;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView image;
+    TextView logo, tag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView4);
+        tag = findViewById(R.id.textView5);
+
+        image.setAnimation(topAnimation);
+        logo.setAnimation(bottomAnimation);
+        tag.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
