202b4383abd1451f0b4e4d5bcf7d77b7a0ab95c9
Shakith <nimsara.shakith@gmail.com>
2020-10-14 19:15:28 +0530

    Sign Up Designed

--- app/src/main/java/com/example/transit/Login.java
+++ app/src/main/java/com/example/transit/Login.java
@@ -2,15 +2,38 @@ package com.example.transit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        signup = findViewById(R.id.signup_btn);
+    }
+
+    public void callSignUpScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(signup, "transition_signup");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/transit/SignUpSecond.java
@@ -0,0 +1,46 @@
+package com.example.transit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUpSecond extends AppCompatActivity {
+
+    TextView title;
+    Button next, login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up_second);
+
+        title = findViewById(R.id.signup_title);
+        next = findViewById(R.id.signup_next_btn);
+        login = findViewById(R.id.signup_login_btn);
+    }
+
+    public void callNextSignupScreen(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), SignupThird.class);
+
+        Pair[] pairs = new Pair[3];
+        pairs[0] = new Pair<View, String>(title, "transition_title");
+        pairs[1] = new Pair<View, String>(next, "transition_next");
+        pairs[2] = new Pair<View, String>(login, "transition_login");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUpSecond.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+    }
+}
